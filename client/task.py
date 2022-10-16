d33d9f88b6af3b5bbb92e8b845bb8dbe4b5b3465
Niclas Eriksen <niclaseriksen@gmx.com>
2022-10-16 03:26:38 +0200

    Forgot to actually use the mask image.

--- client/task.py
+++ client/task.py
@@ -232,7 +232,7 @@ class SDTask():
             seed=self.seed,
             fix_faces=self.fix_faces,
             init_image=self.input_image_file.name if (self.input_image_downloaded and not self.mask_mode_image) else None,
-            mask_image=self.input_image_file.name if (self.input_image_downloaded and self.mask_mode_image) else None,
+            mask_image=self.mask_image_file.name if (self.mask_image_downloaded and self.mask_mode_image) else None,
             init_image_strength=self.input_image_strength,
             upscale=self.upscale,
             tile_mode=self.tileable,
